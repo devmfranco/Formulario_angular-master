fc2ead3929ab42ede7cb1d45eadd3025d153e15b
Michael Rafael Franco Rodriguez <michael.franco@micm.gob.do>
2023-07-03 12:43:28 -0300

    Seccion de area agregada

--- src/app/form-principal/form-principal.component.ts
+++ src/app/form-principal/form-principal.component.ts
@@ -10,7 +10,7 @@ import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 export class FormPrincipalComponent {
 
   nombreCompleto!: string;
-  viceMinisterioDireccion!: string;
+  viceMinisteriodir!: string;
   ubicacionEspecifica!: string;
   numeroContacto!: string;
   ayudaNecesaria!: string;
@@ -24,7 +24,7 @@ validar = false;
     // Aquí puedes realizar acciones con los datos del formulario
     console.log('Formulario enviado');
     console.log('Nombre completo: ' + this.nombreCompleto);
-    console.log('Vice-ministerio o dirección: ' + this.viceMinisterioDireccion);
+    console.log('Vice-ministerio o dirección: ' + this.viceMinisteriodir);
     console.log('Ubicación específica: ' + this.ubicacionEspecifica);
     console.log('Número de contacto o extensión: ' + this.numeroContacto);
     console.log('Ayuda necesaria: ' + this.ayudaNecesaria);
@@ -44,24 +44,57 @@ validar = false;
     Vicomex:'Viceministerio Comercio Exterior',
     VCI:'Viceministerio Comercio Interno',
     VDI:'Viceministerio Desarrollo Industrial',
-    vicePymes:'Viceministerio Fomento Pequeña y Media Emp.',
-    Area:'',
-    Area:'',
-    Area:'',
-    Area:'',
-    Area:'',
-    Area:'',
-    Area:'',
-    Area:'',
-    Area:'',
-    Area:'',
-    Area:'',
-    Area:'',
-    Area:'',
-    Area:'',
-    Area:'',
-    Area:'',
-    Area:'',
+    PYMES:'Viceministerio Fomento Pequeña y Media Emp.',
+    Vizfre:'Viceministerio Zonas Francas y Regimenes Esp.',
+    dirAdm:'Dirección Administrativa',
+    dirFinanciera:'Dirección Administrativa',
+    dirComercioExt:'Dirección Financiera',
+    dirComercioInt:'Dirección Comercio Interno',
+    dirControlGestion:'Dirección Control de Gestión',
+    dirComb:'Dirección de Combustibles',
+    dirCom:'Dirección de Comunicaciones',
+    dirAnalisisEco:'Dirección de Análisis Económico',
+    dirIng:'Dirección de Ingeniería',
+    dirRRHH:'Dirección de Recursos Humanos',
+    dirTecnologia:'Dirección de Tecnología',
+    dirJuridica:'Dirección Jurídica',
+    dirPlanReg:'Dirección Plan Regulador',
+    dirPlanificacion:'Dirección Planificación y Desarrollo',
+    DAIC:'Dirección de Atención Integral al Cliente',
+    dirTratadosComerciales:'Dirección De Administración De Acuerdos Y Tratados Comerciales Internacionales',
+    dirComercioExterior:'',
+    dirEmp:'Dirección De Emprendimiento',
+    DOSAC:'Dirección de Operaciones de Supervisión de Actividades Comerciales',
+    dirEconomiaIndust:'Dirección De Economía Industrial',
+    dirApoyoPymes:'Dirección De Servicios De Apoyo A Las Mipymes',
+    dptoCompras:'Dpto. Compras y Contrataciones',
+    dptoAdmRRHH:'Dpto. de Administración de Recursos Humanos',
+    dptoContab:'Dpto. de Contabilidad',
+    dptoGestionDoc:'Dpto. de Gestión Documental',
+    dptoServGen:'Dpto. de Servicios Generales',
+    deptoTesoreria:'Dpto. de Tesorería',
+    deptoDesarrolloHum:'Dpto. Desarrollo Humano y Planes de Carrera',
+    dptoRelPub:'Dpto. Relaciones Publicas y Pub.',
+    dptoServPerso:'Dpto. Servicio Personal y Relaciones Lab.',
+    dptoPrensa:'Dpto. Prensa',
+    dptoServPresen:'Dpto. de Servicios Presenciales',
+    dptoServLinea:'Dpto. de Servicios en línea',
+    dptoAcceInfo:'Dpto.  de Acceso A La Información',
+    divArchCentral:'División Archivo Central',
+    divCentroDoc:'División Centro de Documentación',
+    divAlmSum:'División de almacén y Suministro',
+    divSegInt:'División de Seguridad Interna',
+    divTransp:'División de Transportación',
+    mntoreoControlInt:'Monitoreo y Control Interno',
+    OAI:'Oficina de Acceso a la Información',
+    OEG:'Oficina de Acceso a la Información',
+    seccCorresp:'Sección de Correspondencia',
+    seccMant:'Sección Mantenimiento',
+    seccMayord:'Sección Mayordomia',
+    consultMedic:'Consultorio Medico',
+    coopemic:'Coopemic',
+
+
 
 
    }
